996d10d3a1cfd7ecfa71014d7a0b05e1bf5746cb
awisu2 <awisu2@gmail.com>
2019-05-17 16:33:40 +0900

    htmlの展開サンプルの追加

--- docs/sample/sample.md
+++ docs/sample/sample.md
@@ -1,4 +1,5 @@
 # sample
 
-- [sass](./sass.md)
+- [sass](./sass.md): sassコンパイル
 - [file-loader](./file-loader.md): 変換をせず任意のフォルダに保存する
+- [html](./html.md): コンパイル対象とは別にhtmlも同時に移動させる(includeタグは自動補完)
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,72 @@
 const path = require('path')
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const isProd = process.env.NODE_ENV === 'production';
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  name: 'app',
-  mode: 'development',
-  entry: {
-    app: './src/js/app.js'
-  },
-  output: {
-    path: path.join(__dirname, 'dist/'),
-    filename: 'js/[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // publicPath
-              publicPath: '../',
-              hmr: !isProd
+const MODE_PRODUCTION = 'production'
+const MODE_DEVELOPMENT = 'development'
+
+module.exports = (env, options) => {
+  // modeによって環境変数がせってされるが、webpack.config内では取得できないのでoptionを参照
+  let isProd = options['mode'] === MODE_PRODUCTION
+
+  return {
+    name: 'app',
+    mode: isProd ? MODE_PRODUCTION : MODE_DEVELOPMENT,
+    entry: {
+      app: './src/js/app.js'
+    },
+    output: {
+      path: path.join(__dirname, 'dist/'),
+      filename: 'js/[name]' + (isProd ? '.[hash]' : '') + '.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // publicPath
+                publicPath: '../',
+                hmr: !isProd
+              }
+            },
+            {
+              loader: "css-loader",
+              options: {
+                url: false,
+                importLoaders: 1
+              }
+            },
+            {
+              loader: "sass-loader"
             }
-          },
-          {
-            loader: "css-loader",
-            options: {
-              url: false,
-              importLoaders: 1
+          ]
+        },
+        {
+          test: /\.(gif|png|jpg|jpeg)$/,
+          use: [
+            "file-loader", {
+              loader: "file-loader",
+              options: {
+                name: './scss/[name].[ext]'
+              }
             }
-          },
-          {
-            loader: "sass-loader"
-          }
-        ]
-      },
-      {
-        test: /\.(gif|png|jpg|jpeg)$/,
-        use: [
-          "file-loader", {
-            loader: "file-loader",
-            options: {
-              name: './scss/[name].[ext]'
-            }
-          }
-        ]
-      }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/[name]' + (isProd ? '.[hash]' : '') + '.css',
+        chunkFilename: 'css/[id]' + (isProd ? '.[hash]' : '') + '.css'
+      }),
+      new HtmlWebpackPlugin([
+        { template: "src/index.html" }
+      ])
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name]' + (isProd ? '.[hash]' : '') + '.css',
-      chunkFilename: 'css/[id]' + (isProd ? '.[hash]' : '') + '.css'
-    }),
-  ]
+  }
 }
 
